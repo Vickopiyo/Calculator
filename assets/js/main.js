100b509c78edd9741d3f63189c0e45b6d14fc5bf
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-06-24 22:33:59 +0300

    Complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,10 @@
 let display=document.querySelector("#display");
 function calcNumbers(x){
     display.value=display.value + x;
-    console.log(x)
+    
 }
 function specKeys(y){
-    console.log(y)
+    
     if(y ==="c"){
         display.value=""
     }else if(y ==="del"){
