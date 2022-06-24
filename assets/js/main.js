e260d12129c267691bec1ba83959ec4db3317afc
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-06-24 23:04:24 +0300

    Done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,12 @@
 // *** Write your code here***
+// 
 let display=document.querySelector("#display");
 function calcNumbers(x){
     display.value=display.value + x;
     
 }
+// DELETE AND CLEAR
 function specKeys(y){
-    
     if(y ==="c"){
         display.value=""
     }else if(y ==="del"){
